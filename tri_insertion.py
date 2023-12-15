53a697174ee5ae19a0487ef1775ea04e6d05e623
smeden-lod <stefmeden@gmail.com>
2023-12-15 11:17:03 +0100

    pretty md

--- tri_insertion.py
+++ tri_insertion.py
@@ -1,6 +1,6 @@
 """
-trie en place un tableau tab d'entiers dans l'ordre croissant. Pour cela, on parcout tab pour k de 1 à len(tab) - 1
-A l'étape k: 
-- tab[:k] est trié ;
-- on insère tab[k] au bon indice pour qu'après insertion tab[:k+1] soit rangé dans l'ordre croissant ;
+trie en place un tableau `tab` d'entiers dans l'ordre croissant. Pour cela, on parcourt `tab` pour `k` de `1` à `len(tab) - 1`
+A l'étape `k`: 
+- `tab[:k]` est trié ;
+- on insère `tab[k]` au bon indice pour qu'après insertion `tab[:k+1]` soit rangé dans l'ordre croissant ;
 """
