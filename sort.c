a2f3b2ae6073290a21fecb79673b5acd455f7adb
smeden-lod <stefmeden@gmail.com>
2023-12-19 06:44:38 +0100

    trying C

---
+++ sort.c
@@ -0,0 +1,21 @@
+// https://www.onlinegdb.com/
+#include <stdio.h>
+#include <assert.h>
+
+void swap(int t[], int n, int i, int j){
+    assert (0 <= i < n);
+    assert (0 <= j < n);
+    int temp = t[i];
+    t[i] = t[j];
+    t[j] = temp;
+}
+
+int main()
+{
+    int t[4] = {1, 3, 7, 9};
+    printf("{%d, %d, %d, %d}\n", t[0], t[1], t[2], t[3]);
+    printf("  swaping...\n");
+    swap(t, 4, 1, 2);
+    printf("{%d, %d, %d, %d}\n", t[0], t[1], t[2], t[3]);
+    return 0;
+}
