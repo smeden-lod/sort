cc14e8e194bd80e98ef2a1acaf3b5d33616c513b
smeden-lod <stefmeden@gmail.com>
2023-12-22 10:17:27 +0100

    sort.c complete

--- sort.c
+++ sort.c
@@ -1,6 +1,4 @@
-// https://www.onlinegdb.com/
 #include <stdio.h>
-#include <assert.h>
 
 void swap(int t[], int n, int i, int j){
     int temp = t[i];
@@ -8,13 +6,39 @@ void swap(int t[], int n, int i, int j){
     t[j] = temp;
 }
 
+void affiche_tableau(int t[], int n){
+    printf("{");
+    for (int i = 0; i <n-1; i++){
+        printf("%d, ", t[i]);
+    }
+    printf("%d}\n", t[n-1]);
+}
 
+int imini(int t[], int n, int k){
+    // t tableau d'entiers de taille n
+    // renvoie l'indice du min dans t à partir de k
+    int indm = k;
+    int m = t[k];
+    for (int i = k+1; i<n ; i++){
+        if (t[i] < m){
+            m = t[i];
+            indm = i;
+        }
+    }
+    return indm;
+}
+
+ 
+void tri_selection(int tab[], int n){
+    for (int i=0; i<n ; i++){
+        swap(tab, n, i, imini(tab, n, i));
+    }
+}
 
 int main(){
-    int t[4] = {1, 3, 7, 9};
-    printf("{%d, %d, %d, %d}\n", t[0], t[1], t[2], t[3]);
-    printf("  swaping...\n");
-    swap(t, 4, 1, 2);
-    printf("{%d, %d, %d, %d}\n", t[0], t[1], t[2], t[3]);
+    int tab[7] = {89, 12, 33, 456, 27, 987, 42};
+    affiche_tableau(tab, 7);
+    tri_selection(tab, 7);
+    affiche_tableau(tab, 7);
     return 0;
 }
